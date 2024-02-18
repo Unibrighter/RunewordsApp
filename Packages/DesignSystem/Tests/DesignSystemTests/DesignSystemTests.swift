2eb61d98d52155aedc8c25cfdf17705d31f7b9c6
Robert Wu <rocklee.au@foxmail.com>
2024-02-18 16:39:39 +1100

    Apply iOS v17 to the RunesData as well; Fix failing unit tests

--- Packages/DesignSystem/Tests/DesignSystemTests/DesignSystemTests.swift
+++ Packages/DesignSystem/Tests/DesignSystemTests/DesignSystemTests.swift
@@ -1,11 +1,11 @@
 import XCTest
+import SwiftUI
 @testable import DesignSystem
 
 final class DesignSystemTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(DesignSystem().text, "Hello, World!")
-    }
+  func testColors() throws {
+    let items = Color.ItemsPalette.allCases
+    let mappedColors = items.map{ Color.itemsColor(color: $0) }.compactMap{ $0 }
+    XCTAssertEqual(mappedColors.count, items.count)
+  }
 }
