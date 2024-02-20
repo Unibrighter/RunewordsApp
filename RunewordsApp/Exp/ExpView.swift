1280a1af782800dea70a3a57c2dfbbc3cc11c6a6
Robert Wu <rocklee.au@foxmail.com>
2024-02-20 22:19:02 +1100

    Finish official SwiftUI intro

--- RunewordsApp/Exp/ExpView.swift
+++ RunewordsApp/Exp/ExpView.swift
@@ -1,58 +1,203 @@
 import SwiftUI
 import DesignSystem
+import MapKit
+import CoreGraphics
 
 struct ExpView: View {
   var body: some View {
-    VStack {
-      GroupBox(
-        label: /*@START_MENU_TOKEN@*/Text("Label")/*@END_MENU_TOKEN@*/,
-        content: {
-          ScrollView {
-            Label(
-              title: { Image(systemName: "42.circle") },
-              icon: { Text("Label") }
-            )
-            Text("👰🏻")
-                .font(.largeTitle)
-                .frame(width: 50, height: 50)
-                .background(Color.orange) // Use a background color that fits your design.
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-                .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.blue, lineWidth: 2))
-                .offset(x:-20, y: -20)
-                .padding(.bottom, 120 )
-            
-            Text("2")
-            Text("3")
-              .foregroundColor(Color.orange)
-            Text("4")
-            Text("5")
-            Text("6")
-            Text("7")
-            Text("8")
-            Text("9")
-            Text("10")
+    VStack{
+      VStack {
+        Map()
+              .frame(height: 300)
+
+
+          Circle()
+          .frame(width: 260)
+              .offset(y: -130)
+              .padding(.bottom, -130)
+        Image(systemName: "leaf")
+            .font(.system(size: 200)) // Adjust the font size to scale the symbol
+            .frame(width: 260, height: 260) // Set the frame for alignment purposes
+
+
+          VStack(alignment: .leading) {
+              Text("Turtle Rock")
+                  .font(.title)
+
+
+              HStack {
+                  Text("Joshua Tree National Park")
+                      .font(.subheadline)
+                Spacer()
+                  Text("California")
+                      .font(.subheadline)
+              }
           }
-          
-      })
-      .font(
-        Font(
-          UIFont(
-            name: "AVQest", 
-            size:
-              UIFont.preferredFont(
-                forTextStyle: UIFont.TextStyle.extraLargeTitle
-              ).pointSize
-          )!
-          )
-        )
+          .padding()
         
-      Text("11")
+      }
+      Spacer()
     }
-  }
+          
+      }
 }
 
 struct ExpView_Previews: PreviewProvider {
   static var previews: some View {
-    ExpView()
+    Group {
+      ExpView()
+      Badge()
+    }
+    
   }
+}
+
+struct Badge: View {
+  var body: some View {
+    ZStack {
+      Group {
+        BadgeBackground()
+        
+        GeometryReader { geometry in
+          BadgeSymbol()
+            .scaleEffect(1.0 / 2.0, anchor: .center)
+//            .position(x: geometry.size.width / 2.0, y: (3.0 / 4.0) * geometry.size.height)
+        }
+      }
+    }.aspectRatio(contentMode: .fit)
+  }
+}
+
+struct BadgeBackground: View {
+    var body: some View {
+        GeometryReader { geometry in
+            Path { path in
+                var width: CGFloat = min(geometry.size.width, geometry.size.height)
+                let height = width
+                let xScale: CGFloat = 0.832
+                let xOffset = (width * (1.0 - xScale)) / 2.0
+                width *= xScale
+                path.move(
+                    to: CGPoint(
+                        x: width * 0.95 + xOffset,
+                        y: height * (0.20 + HexagonParameters.adjustment)
+                    )
+                )
+
+
+                HexagonParameters.segments.forEach { segment in
+                    path.addLine(
+                        to: CGPoint(
+                            x: width * segment.line.x + xOffset,
+                            y: height * segment.line.y
+                        )
+                    )
+
+
+                    path.addQuadCurve(
+                        to: CGPoint(
+                            x: width * segment.curve.x + xOffset,
+                            y: height * segment.curve.y
+                        ),
+                        control: CGPoint(
+                            x: width * segment.control.x + xOffset,
+                            y: height * segment.control.y
+                        )
+                    )
+                }
+            }
+            .fill(.linearGradient(
+                            Gradient(colors: [Self.gradientStart, Self.gradientEnd]),
+                            startPoint: UnitPoint(x: 0.5, y: 0),
+                            endPoint: UnitPoint(x: 0.5, y: 0.6)
+                        ))
+        }.aspectRatio(1, contentMode: .fit)
+            }
+            static let gradientStart = Color(red: 239.0 / 255, green: 120.0 / 255, blue: 221.0 / 255)
+            static let gradientEnd = Color(red: 239.0 / 255, green: 172.0 / 255, blue: 120.0 / 255)
+}
+
+struct HexagonParameters {
+    struct Segment {
+        let line: CGPoint
+        let curve: CGPoint
+        let control: CGPoint
+    }
+
+
+    static let adjustment: CGFloat = 0.085
+
+
+    static let segments = [
+        Segment(
+            line:    CGPoint(x: 0.60, y: 0.05),
+            curve:   CGPoint(x: 0.40, y: 0.05),
+            control: CGPoint(x: 0.50, y: 0.00)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.05, y: 0.20 + adjustment),
+            curve:   CGPoint(x: 0.00, y: 0.30 + adjustment),
+            control: CGPoint(x: 0.00, y: 0.25 + adjustment)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.00, y: 0.70 - adjustment),
+            curve:   CGPoint(x: 0.05, y: 0.80 - adjustment),
+            control: CGPoint(x: 0.00, y: 0.75 - adjustment)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.40, y: 0.95),
+            curve:   CGPoint(x: 0.60, y: 0.95),
+            control: CGPoint(x: 0.50, y: 1.00)
+        ),
+        Segment(
+            line:    CGPoint(x: 0.95, y: 0.80 - adjustment),
+            curve:   CGPoint(x: 1.00, y: 0.70 - adjustment),
+            control: CGPoint(x: 1.00, y: 0.75 - adjustment)
+        ),
+        Segment(
+            line:    CGPoint(x: 1.00, y: 0.30 + adjustment),
+            curve:   CGPoint(x: 0.95, y: 0.20 + adjustment),
+            control: CGPoint(x: 1.00, y: 0.25 + adjustment)
+        )
+    ]
+}
+
+struct BadgeSymbol: View {
+    static let symbolColor = Color(red: 79.0 / 255, green: 79.0 / 255, blue: 191.0 / 255)
+
+
+    var body: some View {
+      
+        GeometryReader { geometry in
+          VStack {
+            Path { path in
+                  let width = min(geometry.size.width, geometry.size.height)
+                  let height = width * 0.75
+                  let spacing = width * 0.030
+                  let middle = width * 0.5
+                  let topWidth = width * 0.226
+                  let topHeight = height * 0.488
+
+
+                  path.addLines([
+                      CGPoint(x: middle, y: spacing),
+                      CGPoint(x: middle - topWidth, y: topHeight - spacing),
+                      CGPoint(x: middle, y: topHeight / 2 + spacing),
+                      CGPoint(x: middle + topWidth, y: topHeight - spacing),
+                      CGPoint(x: middle, y: spacing)
+                  ])
+                  
+                  path.move(to: CGPoint(x: middle, y: topHeight / 2 + spacing * 3))
+                  path.addLines([
+                      CGPoint(x: middle - topWidth, y: topHeight + spacing),
+                      CGPoint(x: spacing, y: height - spacing),
+                      CGPoint(x: width - spacing, y: height - spacing),
+                      CGPoint(x: middle + topWidth, y: topHeight + spacing),
+                      CGPoint(x: middle, y: topHeight / 2 + spacing * 3)
+                  ])
+              }
+            .fill(Self.symbolColor)
+          }
+        }
+    }
 }
---
+++ RunewordsApp/Exp/Hike.swift
@@ -0,0 +1,60 @@
+import Foundation
+
+
+struct Hike: Codable, Hashable, Identifiable {
+    var id: Int
+    var name: String
+    var distance: Double
+    var difficulty: Int
+    var observations: [Observation]
+
+
+    static var formatter = LengthFormatter()
+
+
+    var distanceText: String {
+        Hike.formatter
+            .string(fromValue: distance, unit: .kilometer)
+    }
+
+
+    struct Observation: Codable, Hashable {
+        var distanceFromStart: Double
+
+
+        var elevation: Range<Double>
+        var pace: Range<Double>
+        var heartRate: Range<Double>
+    }
+}
+
+final class ModelData: ObservableObject {
+//    @Published var landmarks: [Landmark] = load("landmarkData.json")
+    var hikes: [Hike] = load("hikeData.json")
+}
+
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+
+
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+        else {
+            fatalError("Couldn't find \(filename) in main bundle.")
+    }
+
+
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+
+
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
